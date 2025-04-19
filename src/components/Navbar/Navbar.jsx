c8687370fce9e439320865f16e072d3b09d6aae7
Nallie Than <zhengyi.than@gmail.com>
2025-04-19 14:46:28 +0800

    Make the Navbar scrollable.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaPaw } from "react-icons/fa6";
 import './Navbar.css'
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
+  const [darkNav, setDarkNav] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY >= 50? setDarkNav(true): setDarkNav(false);
+    })
+  },[])
+
   return (
-    <nav>
+    <nav className={`${darkNav?"dark-nav": ""}`}>
       <div className="nav-left">
         <p>Forever Home Paws</p>
         <FaPaw className='logo'/>
@@ -12,11 +21,31 @@ const Navbar = () => {
 
       <div className="nav-right">
         <ul>
-          <li>Home</li>
-          <li>Adopt a Pet</li>
-          <li>Pet Care Guides</li>
-          <li>Success Stories</li>
-          <li>Contact</li>
+          <li>
+            <Link to='hero' smooth={true} offset={0} duration={400}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to='adopt' smooth={true} offset={-60} duration={400}>
+              Adopt a Pet
+            </Link>
+          </li>
+          <li>
+            <Link to='guides' smooth={true} offset={-60} duration={400}>
+              Pet Care Guides
+            </Link>
+          </li>
+          <li>
+            <Link to='story' smooth={true} offset={-80} duration={400}>
+              Success Stories
+            </Link>
+          </li>
+          <li>
+            <Link to='contact' smooth={true} offset={-80} duration={400}>
+              Contact
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
